1576a11d823cf0f859bd5cb6c40511678ac1b0a5
asbevz <as-bivz@yandex.ru>
2026-06-07 15:09:02 +0300

    Bump to v1.1.1: add donate block in README

--- isdayoff/isdayoff.py
+++ isdayoff/isdayoff.py
@@ -15,7 +15,7 @@ from .typingapi import (
 
 _DELIMITER = "%7C"
 _FORMAT_DATE = "%Y%m%d"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 
 # ── shared helpers ───────────────────────────────────────────────────────────
